bbbb800aabd345caaf4aa328d6654e2c17f136ca
wangtao <wangtao940125@163.com>
2020-11-13 11:52:33 +0800

    fix: sideBar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,22 +13,57 @@ module.exports = {
             { text: 'Mathematics', link: '/mathematics/' },
             { text: 'Visualization', link: '/visualization/guide/' },
           ],
-        sidebar:{
-            '/mathematics/' :[
-                '',
-                'one',
-                'two'
-            ],
-            '/Vue/':[
-                ''
-            ],
-            '/node':[
-                ''
-            ],
-            '/react/':[
-                ''
-            ]
-        }
+        sidebar:[
+            {
+                title:'Vue',
+                path:'/Vue/',
+                sidebarDepth:2
+            },
+            {
+                title:'可视化',
+                path:'/visualization/guide',
+                children:[
+                    {
+                        title:'文档',
+                        path:'/visualization/document/Link.md'
+                    }
+                ]
+            }
+        ]
+        // {
+        //     '/mathematics/' :[
+        //         '',
+        //         'one',
+        //         'two'
+        //     ],
+        //     '/Vue/':[
+        //         ''
+        //     ],
+        //     '/node':[
+        //         ''
+        //     ],
+        //     '/react/':[
+        //         ''
+        //     ],
+            
+        //     '/visualization/':[
+        //         {
+        //             '':'/visualization/guide/'
+        //         },
+        //         {
+        //             'document':'/visulization/document/Link.md'
+        //         }
+        //     ]
+        // }
+        // {
+            //     title: 'Group 1',   // 必要的
+            //     path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            //     collapsable: false, // 可选的, 默认值是 true,
+            //     sidebarDepth: 1,    // 可选的, 默认值是 1
+            //     children: [
+            //       '/'
+            //     ]
+            //   },
     },
     configureWebpack: {
         resolve: {
